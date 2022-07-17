5317d32f197746b5fab05a65a25e53ed76049ad3
Robert Rodgers <robert.rodgers@acloudengineer.com>
2022-07-17 12:59:52 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import random
+import words
+
+def main():
+    print("Welcome to unoriginal Hangman.")
+
+
+
+
+
+
+
+
+
+    the_word = random.choice(words.word_list)
+    print(the_word)
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
